1667e11c91dc5083f3ea55b2f4ff3d13cae03d03
xueser <hadi@gmx.se>
2025-05-02 23:09:16 +0200

    removing select routes to separate blueprint to orginazie the code

--- app.py
+++ app.py
@@ -2,9 +2,8 @@ from flask import Flask, jsonify, render_template, request, redirect, flash, url
 from insert_logic import InsertLogic
 from customer_service import get_customer_detail
 from customer_details_route import customer_bp
+from select_routes import select_bp
 from extensions import mysql
-
-
 
 
 app = Flask(__name__)
@@ -19,8 +18,7 @@ mysql.init_app(app)
 insert_logic = InsertLogic(mysql)
 
 app.register_blueprint(customer_bp)
-
-
+app.register_blueprint(select_bp)
 
 tables = [
     "Adjusters",
@@ -50,180 +48,6 @@ def index():
     return render_template('index.html', tables=tables)
 
 
-@app.route('/customers')
-def customers():
-    cur = mysql.connection.cursor()
-    cur.execute('SELECT customer_id, first_name, last_name, gender, dob, address, city, state, zip, phone, email FROM Customers ')
-    data = cur.fetchall()
-    cur.close()
-    return render_template('customers.html', customers=data)
-
-@app.route('/customers_payment')
-def c_payment():
-    cur = mysql.connection.cursor()
-    cur.execute('SELECT payment_id, customer_id, payment_date, amount, payment_method FROM Customer_Payments')
-    data = cur.fetchall()
-    cur.close()
-    return render_template('customers_payment.html', payments=data)
-
-@app.route('/claims')
-def claims():
-    cur = mysql.connection.cursor()
-    cur.execute('SELECT claim_id, policy_id, claim_type, claim_date, claim_amount, claim_status FROM Claims')
-    data = cur.fetchall()
-    cur.close()
-    return render_template('claims.html', claims=data)
-
-@app.route('/policies')
-def policies():
-    cur = mysql.connection.cursor()
-    cur.execute('SELECT policy_id, customer_id, policy_type, policy_start_date, policy_end_date, premium_amount, policy_status FROM Policies')
-    data = cur.fetchall()
-    cur.close()
-    return render_template('policies.html', policies=data)
-
-@app.route('/agents')
-def agents():
-    cur = mysql.connection.cursor()
-    cur.execute('SELECT agent_id, first_name, last_name, phone, email, address FROM Agents')
-    data = cur.fetchall()
-    cur.close()
-    return render_template('agents.html', agents=data)
-
-@app.route('/adjusters')
-def adjusters():
-    cur = mysql.connection.cursor()
-    cur.execute('SELECT adjuster_id, first_name, last_name, experience_years, phone, email FROM Adjusters')
-    data = cur.fetchall()
-    cur.close()
-    return render_template('adjusters.html', adjusters=data)
-
-@app.route('/auto_policies')
-def auto_policies():
-    cur = mysql.connection.cursor()
-    cur.execute('SELECT auto_policy_id, policy_id, make, model, year, liability_amount, uninsured_motorist, underinsured_motorist, med_pay, collision_damage, named_insured, additional_driver FROM Auto_Policies')
-    data = cur.fetchall()
-    cur.close()
-    return render_template('auto_policies.html', auto_policies=data)
-
-@app.route('/beneficiaries')
-def beneficiaries():
-    cur = mysql.connection.cursor()
-    cur.execute('SELECT beneficiary_id, life_policy_id, beneficiary_name, relationship, percentage FROM Beneficiaries')
-    data = cur.fetchall()
-    cur.close()
-    return render_template('beneficiaries.html', beneficiaries=data)
-
-@app.route('/claim_investigations')
-def claim_investigations():
-    cur = mysql.connection.cursor()
-    cur.execute('SELECT investigation_id, claim_id, investigator_name, investigation_date, findings FROM Claim_Investigations')
-    data = cur.fetchall()
-    cur.close()
-    return render_template('claim_investigations.html', claim_investigations=data)
-
-@app.route('/claim_status_history')
-def claim_status_history():
-    cur = mysql.connection.cursor()
-    cur.execute('SELECT claim_id, status, reason_id, status_date FROM Claim_Status_History')
-    data = cur.fetchall()
-    cur.close()
-    return render_template('claim_status_history.html', claim_status_historys=data)
-
-@app.route('/claim_status_reasons')
-def claim_status_reasons():
-    cur = mysql.connection.cursor()
-    cur.execute('SELECT reason_id, reason_description FROM Claim_Status_Reasons')
-    data = cur.fetchall()
-    cur.close()
-    return render_template('claim_status_reasons.html', sclaim_status_reasons=data)
-
-@app.route('/claims_adjusters')
-def claims_adjusters():
-    cur = mysql.connection.cursor()
-    cur.execute('SELECT claim_id, adjuster_id FROM Claims_Adjusters ')
-    data = cur.fetchall()
-    cur.close()
-    return render_template('claims_adjusters.html',claims_adjusters=data)
-
-@app.route('/customer_payments')
-def customer_payments():
-    cur = mysql.connection.cursor()
-    cur.execute('SELECT payment_id, customer_id, payment_date, amount, payment_method FROM  Customer_Payments')
-    data = cur.fetchall()
-    cur.close()
-    return render_template('customer_payments.html',customer_payments=data)
-
-@app.route('/homeowners_policies')
-def homeowners_policies():
-    cur = mysql.connection.cursor()
-    cur.execute('SELECT homeowners_policy_id, policy_id, liability_amount, property_damage_amount, premium FROM Homeowners_Policies')
-    data = cur.fetchall()
-    cur.close()
-    return render_template('homeowners_policies.html',homeowners_policies=data)
-
-@app.route('/life_insurance_policies')
-def life_insurance_policies():
-    cur = mysql.connection.cursor()
-    cur.execute('SELECT life_policy_id, policy_id, premium_amount, benefit_amount, beneficiary FROM Life_Insurance_Policies')
-    data = cur.fetchall()
-    cur.close()
-    return render_template('life_insurance_policies.html',life_insurance_policies=data)
-
-@app.route('/payments')
-def payments():
-    cur = mysql.connection.cursor()
-    cur.execute('SELECT payment_id, policy_id, payment_date, amount_paid, payment_method FROM Payments')
-    data = cur.fetchall()
-    cur.close()
-    return render_template('payments.html',payments=data)
-
-@app.route('/policy_agents')
-def policy_agents():
-    cur = mysql.connection.cursor()
-    cur.execute('SELECT policy_id, agent_id FROM Policy_Agents')
-    data = cur.fetchall()
-    cur.close()
-    return render_template('policy_agents.html',policy_agents=data)
-
-@app.route('/policy_coverages')
-def policy_coverages():
-    cur = mysql.connection.cursor()
-    cur.execute('SELECT coverage_id, policy_id, coverage_type, coverage_limit, premium FROM Policy_Coverages ')
-    data = cur.fetchall()
-    cur.close()
-    return render_template('policy_coverages.html',policy_coverages=data)
-
-@app.route('/policy_reinsurance')
-def policy_reinsurance():
-    cur = mysql.connection.cursor()
-    cur.execute('SELECT policy_id, contract_id FROM Policy_Reinsurance')
-    data = cur.fetchall()
-    cur.close()
-    return render_template('policy_reinsurance.html',policy_reinsurances=data)
-
-@app.route('/reinsurance_contracts')
-def reinsurance_contracts():
-    cur = mysql.connection.cursor()
-    cur.execute('SELECT contract_id, reinsurer_name, contract_start_date, contract_end_date, contract_amount FROM Reinsurance_Contracts')
-    data = cur.fetchall()
-    cur.close()
-    return render_template('reinsurance_contracts.html',reinsurance_contracts=data)
-
-@app.route('/renters_policies')
-def renters_policies():
-    cur = mysql.connection.cursor()
-    cur.execute('SELECT renters_policy_id, policy_id, liability_amount, property_damage_amount, premium FROM Renters_Policies')
-    data = cur.fetchall()
-    cur.close()
-    return render_template('renters_policies.html',renters_policies=data)
-
-
-#----------------------------------------------------------------------------------------------------------------------------------------
-#  Insert, Update, Delete Sections
-#----------------------------------------------------------------------------------------------------------------------------------------
-#  Insert logic for dashboard
-#----------------------------------------------------------$
 
 #----------------------------------------------------------
 # Dev route
